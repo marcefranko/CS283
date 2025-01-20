f6799092949e9a04ded14a71c57d27e209d65c1d
marcefranko <marce.franco.rivera@gmail.com>
2025-01-20 02:24:16 -0500

    -

--- Week2/1-C-Refresher/stringfun.c
+++ Week2/1-C-Refresher/stringfun.c
@@ -8,20 +8,23 @@
 //prototypes
 void usage(char *);
 void print_buff(char *, int);
-int  setup_buff(char *, char *, int);
+int setup_buff(char *, char *, int);
 
 //prototypes for functions to handle required functionality
-int  count_words(char *, int, int);
+int count_words(char *, int, int);
+
 //add additional prototypes here
+void reverse_string(char *, int);
+void word_print(char *, int);
 
 
-int setup_buff(char *buff, char *user_str, int len){
+int setup_buff(char *buff, char *user_str, int len) {
     //TODO: #4:  Implement the setup buff as per the directions
     char *ptr;  // This will be the pointer to the first character of the user_str
     char *ptr_to_buff;  // This will be a pointer to the buff
     int whitespace_counted; // This will check for double whitespaces in the user_str
     int end_of_string;  // This checks if we already reached the '\0' in the user string
-    int user_str_len;   // This will be the length of the user_str after being copied in the buffer
+    int user_str_len;   // This will be the length of the user_str
 
     ptr = user_str;
     ptr_to_buff = buff;
@@ -37,12 +40,13 @@ int setup_buff(char *buff, char *user_str, int len){
         } else if (*ptr == ' ' || *ptr == '\t') {
             if (whitespace_counted) {
                 ptr++;
+                user_str_len++;
                 continue;
             } else {
                 *ptr_to_buff = ' ';
                 whitespace_counted = 1;
-                user_str_len++;
             }
+            user_str_len++;
         } else {
             *ptr_to_buff = *ptr;
             whitespace_counted = 0;
@@ -88,7 +92,7 @@ int count_words(char *buff, int len, int str_len) {
     word_count = 0;
     word_start = 0;
 
-    for (int i = 0; i < str_len; i++) {
+    while (*buff_pointer != '.') {
         if (!word_start) {
             if (*buff_pointer == ' ') 
                 continue;
@@ -107,6 +111,62 @@ int count_words(char *buff, int len, int str_len) {
 }
 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
+void reverse_string(char *user_str, int str_len) {
+    int end_idx;        //should be length of string - 1
+    int start_idx;
+    char tmp_char;
+
+    end_idx = str_len - 1;
+    start_idx = 0;
+
+    // reversing word:
+    while (end_idx > start_idx) {
+        tmp_char = *(user_str + start_idx);
+        *(user_str + start_idx) = *(user_str + end_idx);
+        *(user_str + end_idx) = tmp_char;
+        start_idx++;
+        end_idx--;
+    }
+    return;
+}
+
+void word_print(char *user_str, int str_len) {
+    int last_char_idx;
+    int wc;
+    int wlen;
+    int word_start;
+
+    last_char_idx = str_len - 1;
+    wc = 0;
+    wlen = 0;
+    word_start = 0;
+
+    for (int i = 0; i < str_len; i++) {
+        if (*(user_str + i) != ' ' && *(user_str + i) != '\t') {
+            // We're in a word, so start or continue it
+            if (word_start == 0) {  // Start of a new word
+                wc++;
+                printf("%d. ", wc);
+                word_start = 1;
+                wlen = 0;  // Reset word length for the new word
+            }
+            // Print the character as part of the word
+            printf("%c", *(user_str + i));
+            wlen++;
+        } else {
+            // We're at a space (or tab), so end the current word if any
+            if (word_start == 1) {
+                printf(" (%d)\n", wlen);  // Print word length
+                word_start = 0;  // End of the current word
+                wlen = 0;  // Reset word length
+            }
+        }
+    }
+
+    if (word_start == 1) {
+        printf(" (%d)\n", wlen);
+    }
+}
 
 int main(int argc, char *argv[]){
 
@@ -139,7 +199,8 @@ int main(int argc, char *argv[]){
     //WE NOW WILL HANDLE THE REQUIRED OPERATIONS
 
     //TODO:  #2 Document the purpose of the if statement below
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
+    //      The purpose of this if statement is to check if the user has inputted the [other options] argument
+    //      This extra argument is important for the '-x' operation
     if (argc < 3){
         usage(argv[0]);
         exit(1);
@@ -152,10 +213,11 @@ int main(int argc, char *argv[]){
     //          return code of 99
     // CODE GOES HERE FOR #3
     buff = malloc(BUFFER_SZ);
+
     // Checking if the memory allocation was successful:
     if (buff == NULL) {
         printf("Memory allocation failure.\n");
-        return -2;  // Returning a Memory allocation failure: -2
+        exit(2);  // exiting due a memory allocation failure. exit code 2
     }
     user_str_len = setup_buff(buff, input_string, BUFFER_SZ);     //see todos
 
@@ -176,6 +238,22 @@ int main(int argc, char *argv[]){
 
         //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
         //       the case statement options
+        case 'r':
+            //TODO: #3. Call reverse string using input_string
+            //          input string should be reversed
+            reverse_string(input_string, user_str_len);
+            printf("Reversed string: %s\n", input_string);
+            break;
+
+        case 'w':
+            printf("Word Print\n----------\n");
+            word_print(input_string, user_str_len);
+            break;
+
+        case 'x':
+            printf("Not Implemented! (Sorry, didn't have time)\n");
+            exit(3);
+
         default:
             usage(argv[0]);
             exit(1);
@@ -183,6 +261,8 @@ int main(int argc, char *argv[]){
 
     //TODO:  #6 Dont forget to free your buffer before exiting
     print_buff(buff,BUFFER_SZ);
+    // freeing buffer:
+    free(buff);
     exit(0);
 }
 
@@ -192,4 +272,7 @@ int main(int argc, char *argv[]){
 //          is a good practice, after all we know from main() that 
 //          the buff variable will have exactly 50 bytes?
 //  
-//          PLACE YOUR ANSWER HERE
+//          I think providing both the buffer and the length of the buffer in the functions
+//          is a good idea because this makes functions more flexible:
+//          There might be cases were our program has more than one buffer and buffer_length,
+//          and by providing these parameters we can reuse the same functions for different buffers.
